6af942c1bf7271632e02685384c768d49983685b
Pim <spelier.pim@gmail.com>
2019-02-28 18:38:23 +0100

    Added template

---
+++ T.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define x first
+#define y second
+#define pb push_back
+#define eb emplace_back
+#define all(v) (v).begin(), (v).end()
+#define rep(i,a,b) for (auto i = (a); i != (b); ++i)
+#define REP(i,n) rep(i,0,n)
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+
+ostream& operator<<(ostream &out, const ii &pr) { return out << '(' << pr.x << ',' << pr.y << ')'; }
+
+void run()
+{
+	
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	run();
+
+	return 0;
+}
